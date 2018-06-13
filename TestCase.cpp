2ff52a66e4d69730a222060baf06829a184dff8c
AleGrins <37497692+AleGrins@users.noreply.github.com>
2018-06-13 15:48:49 +0300

    Update TestCase.cpp

--- TestCase.cpp
+++ TestCase.cpp
@@ -1,19 +1,18 @@
-#include "TestCase.hpp"
-using namespace std;
 
 template<class T>
-void TestCase::check_equal(T const A, T const B)
+TestCase& TestCase::check_equal(T const A, T const B)
 {
     testCount++;
     if(A == B) passed++;
     else{
         out << testName << ": Failure in test #" << testCount << ": " << A << " should equal " << B << "!" << endl;
         failed++;
-    }  
+    } 
+    return *this;
 }
 
 template<class T>
-void TestCase::check_different(T const A, T const B)
+TestCase& TestCase::check_different(T const A, T const B)
 {
     testCount++;
     if(A != B) passed++;
@@ -21,10 +20,11 @@ void TestCase::check_different(T const A, T const B)
         out << testName << ": Failure in test #" << testCount << ": " << A << " should differ from " << B << "!" << endl;
         failed++;
     }  
+    
 }
 
 template<class T>
-void TestCase::check_output(T const A, const string S)
+TestCase& TestCase::check_output(T const A, const string S)
 {
     testCount++;
     ostringstream buf;
@@ -34,10 +34,11 @@ void TestCase::check_output(T const A, const string S)
         out << testName << ": Failure in test #" << testCount << ": string value should be " << S << " but is " << A << "!" << endl;
         failed++;
     } 
+    return *this;
 }
 
 template<class T, class V>
-void TestCase::check_function(V (*func)(T), T A, V B)
+TestCase& TestCase::check_function(V (*func)(T const), T const A, V const B)
 {
     testCount++;
     if(func(A) == B) passed++;
@@ -45,6 +46,7 @@ void TestCase::check_function(V (*func)(T), T A, V B)
         out << testName << ": Failure in test #" << testCount << ": function should return " << B << " but returned " << func(A) << "!" << endl;
         failed++;
     } 
+    return *this;
 }
 
 void TestCase::print()
