38bf6011f2307b306a05ef94bfde95b025dd9f56
AleGrins <37497692+AleGrins@users.noreply.github.com>
2018-06-13 15:49:12 +0300

    Update TestCase.hpp

--- TestCase.hpp
+++ TestCase.hpp
@@ -13,12 +13,12 @@ class TestCase {
     public:
         TestCase(string S, ostream& O) : out(O), testName(S), passed(0), failed(0), testCount(0) {}
         template<class T>
-        void check_equal(T const A, T const B);
+        TestCase& check_equal(T const A, T const B);
         template<class T>
-        void check_different(T const A, T const B);
+        TestCase& check_different(T const A, T const B);
         template<class T, class V>
-        void check_function(V (*func)(T), T A, V B);
+        TestCase& check_function(V (*func)(T const), T const A, V const B);
         template<class T>
-        void check_output(T const A, const string S);
+        TestCase& check_output(T const A, const string S);
         void print();
 };
